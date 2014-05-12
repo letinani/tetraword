9f98c43e055e8a9b5e6b01e6331554b1d240e93c
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-05-12 17:49:25 +0200

    modifs de base à l'installation sur eclipse

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,5 @@ import menus.Interface;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Interface tetraword = new Interface(800, 600);
-		tetraword.pack();
-		tetraword.setVisible(true);
-	}
+
 }
