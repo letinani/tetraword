c509e75e7e90e5a2baa2e9d854447192be287ccb
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-05-12 19:20:15 +0200

    basic button style

--- src/menus/ObservableButton.java
+++ src/menus/ObservableButton.java
@@ -1,5 +1,8 @@
 package menus;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -10,16 +13,19 @@ public class ObservableButton extends Observable {
 	private boolean isClicked;
 	private String text;
 	private JButton button;
-	
+	private Font minecraftia;
+
 	public ObservableButton(String text) {
-		
+		this.minecraftia = TFont.loadFont("data/font/Minecraftia.ttf");
+
 		this.setClicked(false);
 		
 		this.text = text;
 		button = new JButton(this.text);
-		
+		button.setMargin(new Insets(5,5,5,5)); 
+	    button.setFont(minecraftia.deriveFont(Font.PLAIN, 14));
+
 		button.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				updateObservers();
